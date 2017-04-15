bf766420f1905159771f5522c58a41ffc557b755
Kamal <kamaldolikay@gmail.com>
2017-04-16 01:39:47 +0530

    intro to routing

---
+++ client/src/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+module.exports = () => (
+    <div>
+        <h2>Hello KD</h2>
+        <Link to="/repos">Repos</Link>
+    </div>
+)
