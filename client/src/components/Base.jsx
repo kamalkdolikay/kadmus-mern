5ab34f3c9214b13107c9ebdc30bd7181e165758f
KD <kamaldolikay@gmail.com>
2017-04-18 16:50:04 +0530

    react-bootstrap template

---
+++ client/src/components/Base.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link, IndexLink} from 'react-router-dom';
+import { Navbar, NavItem, NavDropDown, MenuItem, Nav, NavDropdown, Jumbotron, Button} from 'react-bootstrap';
+
+const Base = ({ children }) => (
+    <div>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+            <Navbar.Brand>
+                <a href="#">ReactJS</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+            <Nav>
+                <NavItem eventKey={1} href="#">Link</NavItem>
+                <NavItem eventKey={2} href="#">Link</NavItem>
+                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+                <NavItem eventKey={1}><Link to="/login">Log In</Link></NavItem>
+                <NavItem eventKey={2} href="#">Signup</NavItem>
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        <Jumbotron>
+            <center>
+            <h1>Hello, world!</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p><Button bsStyle="primary">Learn more</Button></p>
+            </center>
+        </Jumbotron>
+    </div>
+)
+
+
+export default Base;
