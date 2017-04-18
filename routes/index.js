611ad53660f07adc768efa7e4c749522a863bc26
KD <kamaldolikay@gmail.com>
2017-04-18 16:53:14 +0530

    login routing

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router();
 
 /* GET index page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('index', {
+        title: 'React'
+    });
 });
+
+router.post('/login', (req, res) => {
+    console.log("req", req.body)
+    res.send("success")
+})
 
 export default router;
