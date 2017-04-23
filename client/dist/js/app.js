2df18a0c96a4fcbba11bbb904f1ed66070ced0b3
Kamal <kamaldolikay@gmail.com>
2017-04-23 14:30:38 +0530

    irrelevant

--- client/dist/js/app.js
+++ client/dist/js/app.js
@@ -25310,6 +25310,14 @@
 
 	var _LoginPage2 = _interopRequireDefault(_LoginPage);
 
+	var _DashboardPage = __webpack_require__(482);
+
+	var _DashboardPage2 = _interopRequireDefault(_DashboardPage);
+
+	var _Auth = __webpack_require__(480);
+
+	var _Auth2 = _interopRequireDefault(_Auth);
+
 	var _SignUpForm = __webpack_require__(481);
 
 	var _SignUpForm2 = _interopRequireDefault(_SignUpForm);
@@ -25573,7 +25581,13 @@
 
 	var routes = [{
 	  path: '/',
-	  component: _Base2.default
+	  component: function component() {
+	    if (_Auth2.default.isUserAuthenticated()) {
+	      callback(null, _Base2.default);
+	    } else {
+	      callback(null, _Base2.default);
+	    }
+	  }
 	}, {
 	  path: '/repos/:name',
 	  component: Repo
@@ -46249,6 +46263,113 @@
 	}(_react2.default.Component);
 
 	exports.default = SignUpForm;
+
+/***/ }),
+/* 482 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _Auth = __webpack_require__(480);
+
+	var _Auth2 = _interopRequireDefault(_Auth);
+
+	var _Dashboard = __webpack_require__(483);
+
+	var _Dashboard2 = _interopRequireDefault(_Dashboard);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var DashboardPage = function (_React$Component) {
+	    _inherits(DashboardPage, _React$Component);
+
+	    function DashboardPage(props) {
+	        _classCallCheck(this, DashboardPage);
+
+	        var _this = _possibleConstructorReturn(this, (DashboardPage.__proto__ || Object.getPrototypeOf(DashboardPage)).call(this, props));
+
+	        _this.state = {
+	            secretData: ''
+	        };
+	        return _this;
+	    }
+
+	    _createClass(DashboardPage, [{
+	        key: 'componentDidMount',
+	        value: function componentDidMount() {
+	            var _this2 = this;
+
+	            var xhr = new XMLHttpRequest();
+	            xhr.open('get', '/dashboard');
+	            xhr.setRequestHeader('Content-type', application / x - www - form - urlencoded);
+	            xhr.setRequestHeader('Authorization', 'bearer ' + _Auth2.default.getToken);
+	            xhr.responseType = 'json';
+	            xhr.addEventListener('load', function () {
+	                if (xhr.status === 200) {
+	                    _this2.setState({
+	                        secretData: xhr.response.message
+	                    });
+	                }
+	            });
+	            xhr.send();
+	        }
+	    }, {
+	        key: 'render',
+	        value: function render() {
+	            return _react2.default.createElement(_Dashboard2.default, { secretData: this.state.secretData });
+	        }
+	    }]);
+
+	    return DashboardPage;
+	}(_react2.default.Component);
+
+	exports.default = DashboardPage;
+
+/***/ }),
+/* 483 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var Dashboard = function Dashboard() {
+	    return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	            'p',
+	            null,
+	            'Welcome to the dasbooard'
+	        )
+	    );
+	};
+
+	exports.default = Dashboard;
 
 /***/ })
 /******/ ]);
